6da587a5d435137217e98e26846256ac35b5eeb8
laoliu <2533314379@qq.com>
2022-07-16 20:28:47 +0800

    11

---
+++ src/components/1.买点书练习.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <p>请选择你要购买的书籍</p>
+    <ul>
+    <li v-for="(a,index) in arr" :key="a.name">
+    {{a.name}}
+    <button @click="add(index)">买书</button>
+    </li>
+    </ul>
+    <table border="1" width="500" cellspacing="0">
+      <tr>
+        <th>序号</th>
+        <th>书名</th>
+        <th>单价</th>
+        <th>数量</th>
+        <th>合计</th>
+      </tr>
+      <tr v-for="(n,index) in arr" :key="n.name">
+      <td>{{index++}}</td>
+      <td>{{n.name}}</td>
+      <td>{{n.price}}</td>
+      <td>{{n.count}}</td>
+      <td>{{n.price * n.count}}</td>
+      </tr>
+    </table>
+    <p>总价格为:{{all}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arr: [
+        {
+          name: "水浒传",
+          price: 107,
+          count: 0,
+        },
+        {
+          name: "西游记",
+          price: 192,
+          count: 0,
+        },
+        {
+          name: "三国演义",
+          price: 219,
+          count: 0,
+        },
+        {
+          name: "红楼梦",
+          price: 178,
+          count: 0,
+        },
+      ],
+    };
+  },
+  methods: {
+    add(val){
+      this.arr[val].count++
+    }
+  },
+  computed:{
+    all() {
+      return this.arr.reduce((pre,current) => pre + current.price * current.count,0)
+    }
+  }
+};
+</script>
---
+++ src/components/2.选你爱我求和.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <!-- 无id时, 可以使用index(反正也是就地更新) -->
+    <div
+      style="display: inline-block"
+      v-for="(n,index) in arr" :key="index"
+    >
+      <input type="checkbox" :value="n" v-model="num"  />
+      <span>{{n}}</span>
+    </div>
+    <p>你选中的元素, 累加的值和为:{{all}} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arr: [9, 15, 19, 25, 29, 31, 48, 57, 62, 79, 87],
+      num:[]
+    };
+  },
+  computed:{
+    all() {
+      return this.num.reduce((pre,current) =>{
+        pre += current
+        return pre
+      },0)
+    }
+  }
+};
+</script>
