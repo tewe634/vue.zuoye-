4df0f2a973826325e93b0188a0f9a4bd37a778fa
laoliu <2533314379@qq.com>
2022-07-19 19:21:57 +0800

    完成购物车案例

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,47 @@
+<template>
+  <div id="app">
+  <MyHeader title="购物车" color="skyblue"></MyHeader>
+  <div class="deom">
+    <MyGoods
+    v-for="item in list" :key="item.id"
+    :item="item"
+    ></MyGoods>
+  </div>
+  <MyFooter
+  :list="list"
+  ></MyFooter>
+  </div>
+</template>
 
+<script>
+import MyHeader from '@/components/MyHeader.vue'
+import MyGoods from '@/components/MyGoods.vue'
+import MyFooter from '@/components/MyFooter.vue'
+export default {
+  name:'App',
+  data() {
+    return {
+      list:[],
+    }
+  },
+  components:{
+    MyHeader,
+    MyGoods,
+    MyFooter
+  },
+  created(){
+    this.$axios({
+      url:'/api/cart'
+    }).then(res => this.list = res.data.list)
+  }
+}
+</script>
+
+<style scoped>
+  .deom {
+    margin-top: 40px;
+  }
+  #app {
+    margin-bottom : 40px
+  }
+</style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="my-header" :style="{backgroundColor:color}">{{title}}</div>
+</template>
+
+<script>
+export default {
+    name:'MyHeader',
+    props:{
+      title:{
+        type:String
+      },
+      color:{
+        type:String,
+        default:'pink'
+      }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+  .my-header {
+    height: 45px;
+    line-height: 45px;
+    text-align: center;
+    background-color: #1d7bff;
+    color: #fff;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: 2;
+  }
+</style>
